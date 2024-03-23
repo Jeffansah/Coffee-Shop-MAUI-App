cdeb435834e1e777394034cc4a70706946d04345
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2024-03-23 17:07:05 +0000

    Add, remove from cart with toast messages added

--- MAUIApp/MAUIApp/GlobalUsings.cs
+++ MAUIApp/MAUIApp/GlobalUsings.cs
@@ -6,5 +6,6 @@ global using MAUIApp.ViewModels;
 global using MAUIApp.Pages;
 global using MAUIApp.Services;
 global using CommunityToolkit.Mvvm.Input;
-
+global using CommunityToolkit.Maui.Alerts;
+global using CommunityToolkit.Maui.Core;
 
--- MAUIApp/MAUIApp/ViewModels/DetailsViewModel.cs
+++ MAUIApp/MAUIApp/ViewModels/DetailsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace MAUIApp.ViewModels
 {
 	[QueryProperty(nameof(Coffee), nameof(Coffee))]
@@ -10,6 +11,34 @@ namespace MAUIApp.ViewModels
 
 		[ObservableProperty]
 		private Coffee _coffee;
+
+		[RelayCommand]
+		private void AddToCart()
+		{
+			Coffee.CartQuantity++;
+		}
+
+		[RelayCommand]
+		private void RemoveFromCart()
+		{
+			if (Coffee.CartQuantity > 0)
+			{
+				Coffee.CartQuantity--;
+			}
+		}
+
+		[RelayCommand]
+		private async Task ViewCart()
+		{
+			if (Coffee.CartQuantity > 0)
+			{
+				// go to cart
+			}
+			else
+			{
+				await Toast.Make("Please add your desired quantity to cart.", ToastDuration.Short).Show();
+			}
+		}
 	}
 }
 
